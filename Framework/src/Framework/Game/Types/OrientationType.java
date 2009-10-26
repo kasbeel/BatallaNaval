f13e2dd813aa8896be5f21478953715b41a0f13b
Wladimir A. Jiménez B <wjimenez@kasbeel.cl>
2009-10-26 01:13:49 -0300

    First Upload

---
+++ Framework/src/Framework/Game/Types/OrientationType.java
@@ -0,0 +1,36 @@
+/*
+ * OrientationType.java
+ * 
+ * Copyright (c) 2009 (KTeam).
+ * All rights reserved.
+ * 
+ * http://batallanaval.kasbeel.cl
+ * 
+ * This file is part of BatallaNaval .
+ * 
+ * BatallaNaval  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BatallaNaval  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BatallaNaval .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package Framework.Game.Types;
+
+/**
+ *
+ * @author Kasbeel
+ */
+
+public enum OrientationType {
+    NONE,
+    PORTRAIT,
+    LANDSCAPE
+}
